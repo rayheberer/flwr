351732584dca6b264ded507cae50e76433f661e8
Raymond Heberer <ray.heberer@gmail.com>
2019-09-06 15:05:02 -0700

    Return either a list of calls or the first call evaluated

--- R/flw_branch.R
+++ R/flw_branch.R
@@ -1,1 +1,19 @@
 #' Take an input flow and pass it to multiple pipelines.
+flw_branch <- function(...) {
+  call <- sys.call()
+  
+  pipe_names <- names(call)[2:length(call)]
+  
+  pipe_operations <- lapply(pipe_names, function(name) call[[name]])
+  
+  names(pipe_operations) <- pipe_names
+  
+  function_env <- environment()
+  parent_env <- parent.env(function_env)
+  
+  if (environmentName(parent_env) == "R_GlobalEnv") {
+    return(eval(pipe_operations[[1]]))
+  } else {
+    return(pipe_operations)
+  }
+}
