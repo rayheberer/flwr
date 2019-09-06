9011d6aa256acf69e215532445abc3b94ce16279
Raymond Heberer <ray.heberer@gmail.com>
2019-09-06 15:17:45 -0700

    Add .repl_use argument to specify what to use in interactive environment

--- R/flw_branch.R
+++ R/flw_branch.R
@@ -1,8 +1,12 @@
 #' Take an input flow and pass it to multiple pipelines.
-flw_branch <- function(...) {
+flw_branch <- function(.repl_use = 1, ...) {
   call <- sys.call()
   
   pipe_names <- names(call)[2:length(call)]
+  
+  if (pipe_names[1] == ".repl_use") {
+    pipe_names <- pipe_names[-1]
+  }
   
   pipe_operations <- lapply(pipe_names, function(name) call[[name]])
   
@@ -12,7 +16,7 @@ flw_branch <- function(...) {
   parent_env <- parent.env(function_env)
   
   if (environmentName(parent_env) == "R_GlobalEnv") {
-    return(eval(pipe_operations[[1]]))
+    return(eval(pipe_operations[[.repl_use]]))
   } else {
     return(pipe_operations)
   }
